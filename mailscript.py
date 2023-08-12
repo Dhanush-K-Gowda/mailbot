5854bf1678a21d1932b306f62e4dc4bba264ed8c
Dhanush K Gowda <109449900+Dhanush-K-Gowda@users.noreply.github.com>
2023-08-12 16:20:52 +0530

    Update mailscript.py

--- mailscript.py
+++ mailscript.py
@@ -34,9 +34,7 @@ for remail in df['Email']:
     msg['From'] = sender_email
     msg['To'] = remail
     msg['Subject'] = subject
-    msg.attach(MIMEText(message, 'plain'))
-
-# Establish a secure session with Gmail's outgoing SMTP server using your Gmail account
+    msg.attach(MIMEText(message, 'plain'))
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
 
